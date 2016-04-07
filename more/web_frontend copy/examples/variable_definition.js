07033f95156dfbf4c1ae0bc5e548807e95306bb4
jeansung <jsung@hmc.edu>
2016-04-07 13:57:06 -0700

    merged work from last week into 1 file, moved javascript to separate files

---
+++ more/web_frontend copy/examples/variable_definition.js
@@ -0,0 +1,114 @@
+  $("#ind_var").alpaca({
+      "schema": {
+          "title": "Define your independent variable: ",
+          "type": "object",
+          "properties": {
+              "name": {
+                  "type": "string",
+                  "title": "Name",
+                  "default": "tax",
+              },
+              "macrotype": {
+                "type": "String",
+                "title": "Macrotype",
+                "enum": ["independent variable"]
+
+              },
+              "microtype": {
+                  "type": "String",
+                  "title": "Microtype",
+                  "enum": ['TkAdjustable number', 'TkToggle TkSwitch'],
+                  "required": true 
+
+              },
+              "datavar": {
+                "type": "string",
+                "title": "Variable name",
+                "required": true              
+              },
+              "datamin": {
+                "title": "data min",
+                "type": "integer",
+                "required": true,
+              
+              },
+              "datamax": {
+                "type": "integer",
+                "title": "data max",
+                "required": true
+              },
+              "datastep": {
+                "type": "integer", 
+                "title": "data step", 
+                "default":  1
+              },
+              "displaytext": {
+                "type": "string", 
+                "title": "display text",
+                "default": ""
+              },
+              "datavalues": {
+                "type": "string", 
+                "title": "data values"
+              }
+          },
+          "dependencies": {
+            "datamin": ["microtype"],
+            "datamax": ["microtype"],
+            "datastep": ["microtype"],
+            "displaytext": ["microtype"], 
+            "datavalues": ["microtype"]
+          }
+      },
+      "options": {
+        "fields": {
+          "name": {
+            "disabled":true
+          },
+          "macrotype": {
+            "type": "select",
+            "disabled":true,
+            "removeDefaultNone":true
+          },
+          "microtype": {
+            "type": "select",
+            "removeDefaultNone":false,
+            "noneLabel":"--Select--",
+            "emptySelectFirst": true
+          
+          },
+          "datavar":{
+            "disallowOnlyEmptySpaces": true,
+            "helper": "Enter a name for the variable, cannot be empty."
+          },
+          "datamin": {
+            "dependencies": {
+              "microtype": "TkAdjustable number"
+            }
+          },
+          "datamax": {
+            "dependencies": {
+              "microtype": "TkAdjustable number"
+            }
+          },
+          "datastep": {
+            "dependencies": {
+              "microtype": "TkAdjustable number"
+            }
+          },
+          "displaytext": {
+            "disabled":true,
+            "dependencies": {
+              "microtype": "TkAdjustable number"
+            }
+          },
+          "datavalues": {
+            "dependencies": {
+              "microtype": "TkToggle TkSwitch"
+            }
+          }
+        }
+      }
+  });
+
+
---
+++ web_frontend/javascript/text_editor.js
@@ -0,0 +1,77 @@
+var editor = new Quill('#editor-container', {});
+  
+function textDisplayControl($scope) {
+$scope.getParseText = function() {
+  var rawText = editor.getText();
+
+  // Constants for processing text
+  const MATH_BEGIN = ["(", "[", "{"];
+  const MATH_END = [")", "]", "}"];
+  const CONST_MATCH = "()";
+  const IND_MATCH = "[]";
+  const DEP_MATCH = "{}";
+
+  // Variables for processing text
+  var currentChar = "";
+  var currentString = "";
+  var variableMode = false;
+
+  // Variables that contain the pieces after processing
+  var pieces = [];
+  var indVar = [];
+  var depVar = [];
+  var consts = [];
+
+  for (var i = 0, len = rawText.length; i < len; i++) {
+    currentChar = rawText[i];
+    if (variableMode) {
+      currentString = currentString.concat(currentChar);
+      var isEndVariable = MATH_END.indexOf(currentChar) > -1;
+      if (isEndVariable) {
+        pieces.push(currentString);
+        var beginBracket = currentString[0];
+        var endBracket = currentString[currentString.length-1];
+        var brackets = beginBracket.concat(endBracket);
+        if (brackets === IND_MATCH) {
+          indVar.push(currentString);
+        } else if (brackets === DEP_MATCH) {
+          depVar.push(currentString);
+        } else if (brackets === CONST_MATCH) {
+          consts.push(currentString);
+        } else {
+          console.log("Error -- Bracket Mismatched.");
+        }
+        // Reset string 
+        currentString = ""
+        variableMode = false;
+      } 
+    } else {
+      var isBeginVariable = MATH_BEGIN.indexOf(currentChar) > -1;
+      if (isBeginVariable) {
+        pieces.push(currentString);
+        currentString = "";
+        currentString = currentString.concat(currentChar);
+        variableMode = true;
+      } else {
+        currentString = currentString.concat(currentChar);
+        var isLastChar = (i === (len -1));
+        if (isLastChar) {
+          pieces.push(currentString);
+        }
+      }
+    }
+  }
+
+  console.log("pieces: ", pieces);
+  console.log("ind var list: ", indVar);
+  console.log("dep var list: ", depVar);
+  console.log("consts list: ",consts);
+
+  // Variables to display pieces
+  $scope.piecesList = pieces;
+  $scope.indVarList = indVar;
+  $scope.depVarList = depVar;
+  $scope.constsList = consts;
+
+};
+}; 
