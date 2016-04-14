9d599119129c2540d85bceeaeed3f2ecec9c9a9e
jeansung <jsung@hmc.edu>
2016-04-13 22:26:11 -0700

    added in support for serialize for other types

--- web_frontend/form/constants/variable_definition.js
+++ web_frontend/form/constants/variable_definition.js
@@ -1,4 +1,18 @@
 $("#consts").alpaca({
     "schemaSource": "/form/constants/schema.json",
-    "optionsSource": "/form/constants/options.json"
+    "optionsSource": "/form/constants/options.json",
+    "options": {
+        "form": {
+        "buttons": {
+          "submit": {
+              "title": "Serialize",
+              "click": function() {
+                  var value = this.getValue();
+                  console.log(value);
+                  alert(JSON.stringify(value, null, "  "));
+              }
+          }
+        }
+      },
+  }
 });
--- web_frontend/form/dependent_variable/variable_definition.js
+++ web_frontend/form/dependent_variable/variable_definition.js
@@ -1,6 +1,20 @@
 $("#dep_var").alpaca({
     "schemaSource": "/form/dependent_variable/schema.json",
-    "optionsSource": "/form/dependent_variable/options.json"
+    "optionsSource": "/form/dependent_variable/options.json",
+    "options": {
+        "form": {
+        "buttons": {
+          "submit": {
+              "title": "Serialize",
+              "click": function() {
+                  var value = this.getValue();
+                  console.log(value);
+                  alert(JSON.stringify(value, null, "  "));
+              }
+          }
+        }
+      },
+  }
 });
 
 
